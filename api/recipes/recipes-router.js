8924d9a9dda4dcf98e6650db2369c5aa2067bfdc
Chaunceyo <chaunceyogletree725@gmail.com>
2024-08-11 01:33:11 -0400

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -4,8 +4,7 @@ const Recipe = require("./recipes-model")
 router.get('/:recipe_id', (req,res,next) => {
     Recipe.getRecipeById(req.params.recipe_id)
         .then(recipe => {
-
-            //res.status(200).json(recipe)
+            res.status(200).json(recipe)
         })
         .catch(next)
 })
